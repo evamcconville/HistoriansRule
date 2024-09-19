b58af63be38fd200d0618e95944572bbc4cd7fa7
Luisde Cespedes <lewisofbristol@gmail.com>
2024-09-19 14:25:01 +0200

    first mvp done

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-import MistralClient from '@mistralai/mistralai';
+import { Mistral } from '@mistralai/mistralai';
 
 const apiKey = process.env.MISTRAL_API_KEY || 'Q9wYLD8kvpWmXkYdpeDYGjpwfEbvAxOV';
-const client = new MistralClient(apiKey);
 
-console.log('Mistral Client:', client);
+const client = new Mistral({apiKey: apiKey});
+
+const chatResponse = await client.chat.complete({
+  model: 'mistral-large-latest',
+  messages: [
+    {role: 'system', content: 'You are Spanish, reply only in spanish, you dont know any other language.'},
+    {role: 'user', content: 'How are you'}
+  ],
+  temperature: 0.5
+});
+
+console.log('Chat:', chatResponse.choices[0].message.content);
